32d656f3db3232ddc623132343c76bd1612432ea
aidenbux <81707378+aidenbux@users.noreply.github.com>
2021-04-28 23:39:30 -0600

    Add files via upload

---
+++ w2/3w/StudentLab02/scripts/script.js
@@ -0,0 +1,9 @@
+/*
+    Student Name: Aiden Buxton
+    File Name: script.js
+    Date: 4/28/21
+*/
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    ('.hero').height($(window).height());
+});
